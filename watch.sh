a50ab6fa3fab49ba08a09b51b0982f06d9725557
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-07-05 16:28:30 +0800

    complete research

--- watch.sh
+++ watch.sh
@@ -6,5 +6,5 @@ while true
 do
     inotifywait -e modify README.md || true
     pandoc -t revealjs -s -o README.html --metadata-file README.yaml README.md
-    pandoc -t pdf -s --toc -o README.pdf --metadata-file README.yaml README.md
+    pandoc -t pdf -s --toc --pdf-engine=xelatex -o README.pdf --metadata-file README.yaml README.md
 done
